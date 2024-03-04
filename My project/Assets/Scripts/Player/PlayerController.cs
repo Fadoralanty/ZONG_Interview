1a5e6d0108e453235ef4dd244fe5b7516a520b1b
Julian Lanty <julianemanueltejo@gmail.com>
2024-03-04 02:37:56 -0300

    Player controller as first-person point of view

---
+++ My project/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : Entity
+{
+    [SerializeField] private float _moveSpeeed;
+    [SerializeField] private float _sprintSpeeed;
+    [SerializeField] private Transform _orientation;
+    protected  override void Awake()
+    {
+        base.Awake();
+        speed = _moveSpeeed;
+    }
+    private void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        Vector3 movedir = _orientation.right * hor + _orientation.forward * ver;
+        Move(movedir.normalized);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = _sprintSpeeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = _moveSpeeed;
+        }
+    }
+    
+}
